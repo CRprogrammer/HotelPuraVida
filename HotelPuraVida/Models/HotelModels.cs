4d2c1c12c67589781f54b133014d56ac7a8f5508
maria-2112 <maria.as21@yahoo.com>
2016-08-01 03:18:04 -0600

    Validación de datos de la tabla Hotel

--- HotelPuraVida/Models/HotelModels.cs
+++ HotelPuraVida/Models/HotelModels.cs
@@ -10,8 +10,17 @@ namespace HotelPuraVida.Models
     {
         [Key]
         public int HotelID { get; set; }
+
+        [Display(Name="Hotel")]
+        [Required(ErrorMessage="You must enter {0}")]
         public string Name { get; set; }
+
+        [Display(Name="Compañia")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public string Company { get; set; }
+
+        [Display(Name="Estrellas")]
+        [Required(ErrorMessage = "You must enter {0}")]
         public int Rating { get; set; }
        
 
