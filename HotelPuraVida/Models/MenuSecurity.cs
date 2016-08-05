76ba290b1efaf9b602ae7d0ee68538655564ad83
CRprogrammer <oscarm.gonzalez@outlook.com>
2016-08-05 06:38:35 -0500

    Cambios en layout e imagenes

---
+++ HotelPuraVida/Models/MenuSecurity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace HotelPuraVida.Models
+{
+    public class MenuSecurity
+    {
+        public static bool DashboardVisible
+        {
+            get
+            {
+                return
+                   HttpContext.Current.User != null &&
+                   HttpContext.Current.User.Identity.IsAuthenticated;
+            }
+        }
+    }
+}
